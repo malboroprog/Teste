fbadcb8c3dc30ea21debb15f8d54ae24d5c4d82d
Seu Nome <seu@email.com>
2026-07-25 22:22:18 -0300

    chore: finalize ruff/black/isort changes (skip hooks)

--- Doc/html/_downloads/6dc1f3f4f0e6ca13cb42ddf4d6cbc8af/tzinfo_examples.py
+++ Doc/html/_downloads/6dc1f3f4f0e6ca13cb42ddf4d6cbc8af/tzinfo_examples.py
@@ -1,5 +1,4 @@
-import datetime as dt
-
+﻿import datetime as dt
 # A class capturing the platform's idea of local time.
 # (May result in wrong values on historical times in
 #  timezones where UTC offset and/or the DST rules had
@@ -27,9 +26,8 @@ class LocalTimezone(dt.tzinfo):
         args = time.localtime(stamp)[:6]
         dst_diff = DSTDIFF // SECOND
         # Detect fold
-        fold = (args == time.localtime(stamp - dst_diff))
-        return dt.datetime(*args, microsecond=when.microsecond,
-                           tzinfo=self, fold=fold)
+        fold = args == time.localtime(stamp - dst_diff)
+        return dt.datetime(*args, microsecond=when.microsecond, tzinfo=self, fold=fold)
 
     def utcoffset(self, when):
         if self._isdst(when):
@@ -47,9 +45,17 @@ class LocalTimezone(dt.tzinfo):
         return time.tzname[self._isdst(when)]
 
     def _isdst(self, when):
-        tt = (when.year, when.month, when.day,
-              when.hour, when.minute, when.second,
-              when.weekday(), 0, 0)
+        tt = (
+            when.year,
+            when.month,
+            when.day,
+            when.hour,
+            when.minute,
+            when.second,
+            when.weekday(),
+            0,
+            0,
+        )
         stamp = time.mktime(tt)
         tt = time.localtime(stamp)
         return tt.tm_isdst > 0
@@ -59,6 +65,7 @@ Local = LocalTimezone()
 
 
 # A complete implementation of current DST rules for major US time zones.
+
 
 def first_sunday_on_or_after(when):
     days_to_go = 6 - when.weekday()
@@ -77,19 +84,19 @@ def first_sunday_on_or_after(when):
 #
 # In the US, since 2007, DST starts at 2am (standard time) on the second
 # Sunday in March, which is the first Sunday on or after Mar 8.
-DSTSTART_2007 = dt.datetime(1, 3, 8, 2)
+DSTSTART_2007 = dt.datetime(1, 3, 8, 2)  # noqa: DTZ001
 # and ends at 2am (DST time) on the first Sunday of Nov.
-DSTEND_2007 = dt.datetime(1, 11, 1, 2)
+DSTEND_2007 = dt.datetime(1, 11, 1, 2)  # noqa: DTZ001
 # From 1987 to 2006, DST used to start at 2am (standard time) on the first
 # Sunday in April and to end at 2am (DST time) on the last
 # Sunday of October, which is the first Sunday on or after Oct 25.
-DSTSTART_1987_2006 = dt.datetime(1, 4, 1, 2)
-DSTEND_1987_2006 = dt.datetime(1, 10, 25, 2)
+DSTSTART_1987_2006 = dt.datetime(1, 4, 1, 2)  # noqa: DTZ001
+DSTEND_1987_2006 = dt.datetime(1, 10, 25, 2)  # noqa: DTZ001
 # From 1967 to 1986, DST used to start at 2am (standard time) on the last
 # Sunday in April (the one on or after April 24) and to end at 2am (DST time)
 # on the last Sunday of October, which is the first Sunday
 # on or after Oct 25.
-DSTSTART_1967_1986 = dt.datetime(1, 4, 24, 2)
+DSTSTART_1967_1986 = dt.datetime(1, 4, 24, 2)  # noqa: DTZ001
 DSTEND_1967_1986 = DSTEND_1987_2006
 
 
@@ -103,7 +110,7 @@ def us_dst_range(year):
     elif 1966 < year < 1987:
         dststart, dstend = DSTSTART_1967_1986, DSTEND_1967_1986
     else:
-        return (dt.datetime(year, 1, 1), ) * 2
+        return (dt.datetime(year, 1, 1),) * 2  # noqa: DTZ001
 
     start = first_sunday_on_or_after(dststart.replace(year=year))
     end = first_sunday_on_or_after(dstend.replace(year=year))
@@ -172,7 +179,7 @@ class USTimeZone(dt.tzinfo):
             return dst_time
 
 
-Eastern  = USTimeZone(-5, "Eastern",  "EST", "EDT")
-Central  = USTimeZone(-6, "Central",  "CST", "CDT")
+Eastern = USTimeZone(-5, "Eastern", "EST", "EDT")
+Central = USTimeZone(-6, "Central", "CST", "CDT")
 Mountain = USTimeZone(-7, "Mountain", "MST", "MDT")
-Pacific  = USTimeZone(-8, "Pacific",  "PST", "PDT")
+Pacific = USTimeZone(-8, "Pacific", "PST", "PDT")
