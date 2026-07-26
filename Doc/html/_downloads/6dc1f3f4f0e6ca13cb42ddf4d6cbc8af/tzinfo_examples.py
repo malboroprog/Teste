f8f750203c0d17a98bab9a3672576d8556453e14
Seu Nome <seu@email.com>
2026-07-25 23:10:31 -0300

    chore: finalize formatting and lint fixes (forced)

--- Doc/html/_downloads/6dc1f3f4f0e6ca13cb42ddf4d6cbc8af/tzinfo_examples.py
+++ Doc/html/_downloads/6dc1f3f4f0e6ca13cb42ddf4d6cbc8af/tzinfo_examples.py
@@ -1,4 +1,4 @@
-﻿import datetime as dt
+import datetime as dt
 # A class capturing the platform's idea of local time.
 # (May result in wrong values on historical times in
 #  timezones where UTC offset and/or the DST rules had
